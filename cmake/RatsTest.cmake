1e01c88913db1e2ba78a2301725d14855758427d
Jon Lanz <jon.lanz@dreamworks.com>
2023-05-05 15:17:17 -0700

    Change test naming convention

--- cmake/RatsTest.cmake
+++ cmake/RatsTest.cmake
@@ -11,10 +11,15 @@ function(add_rats_test test_basename)
     set(rats_assets_dir ${PROJECT_SOURCE_DIR}/assets/)
 
     foreach(exec_mode ${ARG_EXEC_MODES})
-        set(test_name ${test_basename}_${exec_mode})
 
-        set(image_name "${test_name}.exr")
-        set(diff_name "${test_name}_diff.exr")
+        set(canonical_test_name "rats_${exec_mode}_canonical_${test_basename}")
+        set(canonical_denoise_test_name "rats_${exec_mode}_canonical_denoise_${test_basename}")
+        set(render_test_name "rats_${exec_mode}_render_${test_basename}")
+        set(render_denoise_test_name "rats_${exec_mode}_render_denoise_${test_basename}")
+        set(diff_test_name "rats_${exec_mode}_diff_${test_basename}")
+
+        set(image_name "${test_basename}_${exec_mode}.exr")
+        set(diff_name "${test_basename}_${exec_mode}_diff.exr")
 
         # create arg list
         set(arg_list "")
@@ -32,51 +37,51 @@ function(add_rats_test test_basename)
         # add test to generate canonicals
         set(canonical_arg_list ${arg_list})
         list(APPEND canonical_arg_list -out ${RATS_CANONICAL_PATH}/${image_name})
-        add_test(NAME ${test_name}_canonicals
+        add_test(NAME ${canonical_test_name}
             WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
             COMMAND moonray -rdla_set "rats_assets_dir" "\"${rats_assets_dir}\"" ${canonical_arg_list}
         )
-        set_tests_properties(${test_name}_canonicals PROPERTIES
+        set_tests_properties(${canonical_test_name} PROPERTIES
             LABELS "canonicals"
             ENVIRONMENT RDL2_DSO_PATH=${rdl2_dso_path}
         )
         # add test to denoise canonical?
         if(ARG_DENOISE)
-            add_test(NAME ${test_name}_canonicals_denoise
+            add_test(NAME ${canonical_denoise_test_name}
                 WORKING_DIRECTORY ${CMAKE_BINARY_DIR}
                 COMMAND denoise -mode oidn -in ${RATS_CANONICAL_PATH}/${image_name} -out ${RATS_CANONICAL_PATH}/${image_name}
             )
-            set_tests_properties(${test_name}_canonicals_denoise PROPERTIES
+            set_tests_properties(${canonical_denoise_test_name} PROPERTIES
                 LABELS "canonicals"
-                DEPENDS ${test_name}_canonicals
+                DEPENDS ${canonical_test_name}
             )
         endif()
 
         # add test to render result
         set(render_arg_list ${arg_list})
         list(APPEND render_arg_list -out ${CMAKE_CURRENT_BINARY_DIR}/${image_name})
-        add_test(NAME ${test_name}_render
+        add_test(NAME ${render_test_name}
             WORKING_DIRECTORY ${ARG_WORKING_DIRECTORY}
             COMMAND moonray -rdla_set "rats_assets_dir" "\"${rats_assets_dir}\"" ${render_arg_list}
         )
-        set_tests_properties(${test_name}_render PROPERTIES
+        set_tests_properties(${render_test_name} PROPERTIES
             LABELS "rats"
             ENVIRONMENT RDL2_DSO_PATH=${rdl2_dso_path}
         )
         # add test to denoise rendered result?
         if(ARG_DENOISE)
-            add_test(NAME ${test_name}_render_denoise
+            add_test(NAME ${render_denoise_test_name}
                 WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
                 COMMAND denoise -mode oidn -in ${image_name} -out ${image_name}
             )
-            set_tests_properties(${test_name}_render_denoise PROPERTIES
+            set_tests_properties(${render_denoise_test_name} PROPERTIES
                 LABELS "rats"
-                DEPENDS ${test_name}_render
+                DEPENDS ${render_test_name}
             )
         endif()
 
         # add test to diff the result with the canonical via oiiotool
-        add_test(NAME ${test_name}_diff
+        add_test(NAME ${diff_test_name}
             WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
             COMMAND ${IDIFF}
                 -o ${diff_name}
@@ -84,9 +89,9 @@ function(add_rats_test test_basename)
                 -abs
                 ${image_name} ${RATS_CANONICAL_PATH}/${image_name}
         )
-        set_tests_properties(${test_name}_diff PROPERTIES
+        set_tests_properties(${diff_test_name} PROPERTIES
             LABELS "rats;diff"
-            DEPENDS ${test_name}_render
+            DEPENDS ${render_test_name}
         )
     endforeach()
 endfunction()
