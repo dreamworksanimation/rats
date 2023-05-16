d1f37e23a7f8cb402898decb1d622a62891c7bed
Jon Lanz <jon.lanz@dreamworks.com>
2023-05-15 17:06:55 -0700

    Wip exrheader comparison

--- cmake/CompareExrHeaders.cmake
+++ cmake/CompareExrHeaders.cmake
@@ -18,51 +18,62 @@ macro(run_exrheader exr_file output_var)
     endif()
 endmacro()
 
+macro(split_resume_history header_in header_out json_out)
+    set(resume_history_pattern "(resumeHistory \\(type string\\): \"{)(.*)(}\")")
+
+    # find it and store it in the json var
+    string(REGEX MATCH ${resume_history_pattern} resume_history ${header_in})
+
+    if(DEFINED resume_history)
+        # capture the JSON stored in the resumeHistory field
+        string(REGEX REPLACE "(resumeHistory \\(type string\\): \")(.*)(\"$)" "\\2" ${json_out} ${resume_history})
+
+        # strip the resumeHistory field from the canonical header
+        string(REGEX REPLACE "(.*)(${resume_history_pattern})(.*)" "\\1\\6" ${header_out} ${header_in})
+    endif()
+endmacro()
+
+macro(filter_header header_in header_out)
+    # turn the header into a list so we can filter line by line
+    string(REPLACE "\n" ";" header_lines ${header_in})
+
+    set(${header_out} "")
+    foreach(line ${header_lines})
+        # strip blank lines
+        if(NOT line)
+            continue()
+        endif()
+
+        # strip opening "file /path/to/file.exr" line since it will be different for each header
+        if(line MATCHES "^file (.*)exr:$")
+            continue()
+        endif()
+
+        # strip "capDate" line because it contains a timestamp and will always be different
+        if(line MATCHES "^capDate")
+            continue()
+        endif()
+
+        string(APPEND ${header_out} "${line}\n")
+    endforeach()
+endmacro()
+
 if(EXISTS ${CANONICAL_EXR} AND EXISTS ${RESULT_EXR})
     run_exrheader(${CANONICAL_EXR} canonical_header)
     run_exrheader(${RESULT_EXR} result_header)
 
-    # strip out and save our custom JSON content from the header string, if found
+    split_resume_history(${canonical_header} canonical_header canonical_json)
+    split_resume_history(${result_header} result_header result_json)
 
-    # this pattern matches the chunk of the header containing the resumeHistory field
-    set(resume_history_pattern "(resumeHistory \\(type string\\): \"{)(.*)(}\")")
+    filter_header(${canonical_header} canonical_header_filtered)
+    filter_header(${result_header} result_header_filtered)
 
-    # find it and store it in the json var
-    string(REGEX MATCH ${resume_history_pattern} resume_history ${canonical_header})
-    if(DEFINED resume_history)
-
-        # strip the 'resumeHistory' exr header field keeping only what's inside
-        # (we want only the actual JSON data stored within the field)
-        string(REGEX REPLACE "(resumeHistory \\(type string\\): \")(.*)(\"$)" "\\2" json ${resume_history})
-
-        string(REGEX REPLACE "(.*)(${resume_history_pattern})(.*)" "\\1\\6" out ${canonical_header})
-
-        # turn what's left of the header into a list so we can filter line by line
-        # keeping only the fields we want
-        string(REPLACE "\n" ";" canonical_header_lines ${out})
-        set(canonical_header_filtered "")
-        foreach(line ${canonical_header_lines})
-            # strip opening "file /path/to/file.exr" line since it will be different
-            # between the canonical and the current result
-            if(line MATCHES "^file (.*)exr:$")
-                continue()
-            endif()
-
-            # strip "capDate" line it contains a timestamp
-            if(line MATCHES "^capDate")
-                continue()
-            endif()
-
-            string(APPEND canonical_header_filtered "${line}\n")
-        endforeach()
-
-        message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
-        message("${json}")
-        message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
-        message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
-        message("${canonical_header_filtered}")
-        message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
-    endif()
+    message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+    message("${canonical_json}")
+    message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
+    message("@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@\n")
+    message("${canonical_header_filtered}")
+    message("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%\n")
 else()
     message(FATAL_ERROR "canonical or result not found")
 endif()
